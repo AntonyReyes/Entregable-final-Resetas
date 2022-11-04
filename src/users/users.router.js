e9390b7a90f2d84568575b78e7e4f4b12c19a73a
AntonyReyes <areyesf20_2@unc.edu.pe>
2022-11-03 23:22:30 -0500

    first commit

---
+++ src/users/users.router.js
@@ -0,0 +1,54 @@
+const router = require('express').Router()
+const passport = require('passport')
+const adminValidate = require('../middlewares/role.middleware')
+const userServices = require('./users.services')
+const {getUserRecipes} = require('../recipes/recipes.services')
+
+require('../middlewares/auth.middleware')(passport)
+
+
+
+
+router.get('/', userServices.getAllUsers)
+
+
+
+//? Ruta de informacion propia del usuario loggeado
+router.route('/me')
+    .get(
+        passport.authenticate('jwt', {session: false}),
+        userServices.getMyUser)
+    .patch(
+        passport.authenticate('jwt', {session: false}),
+        userServices.patchMyUser
+    )
+    .delete(
+        passport.authenticate('jwt', {session: false}),
+        userServices.deleteMyUser
+    )
+
+
+router.get('/me/my_recipes', 
+        passport.authenticate('jwt', {session: false}),
+        getUserRecipes
+)
+
+//? /api/v1/users/:id
+router.route('/:id')
+    .get(userServices.getUserById)
+    .patch(
+        passport.authenticate('jwt', {session: false}),
+        adminValidate,
+        userServices.patchUser
+    )
+    .delete(
+        passport.authenticate('jwt', {session: false}),
+        adminValidate,
+        userServices.deleteUser
+    )
+
+
+
+
+
+module.exports = router
